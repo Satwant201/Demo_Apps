53edd96de7e7327f448ad09b8ee7df53fc3484dd
Satwant201 <54273416+Satwant201@users.noreply.github.com>
2019-09-24 11:11:49 +0530

    Create RshinyApp.r

---
+++ RshinyApp.r
@@ -0,0 +1,19 @@
+########### Rshiny App 
+
+# This is a sample R/Shiny script to  App publishing functionality
+
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    hist(rnorm(input$obs), col = 'darkgray', border = 'white')
+  })
+}
+ 
+ui <- fluidPage(sidebarLayout(sidebarPanel(
+  sliderInput(
+    "obs", "Number of observations:", min = 10, max = 500, value = 100
+  )
+),
+ 
+mainPanel(plotOutput("distPlot"))))
+ 
+shinyApp(ui = ui, server = server)
